e9b445b6a9d43e968ffb51a061fcb6ac4613b072
Mitesh Shah <mitesh@miteshshah.com>
2016-06-24 16:05:54 +0530

    Refactored get request and class repr.

--- pyurbandict/main.py
+++ pyurbandict/main.py
@@ -1,6 +1,6 @@
 import requests
 
-UD_GET_URL = "http://api.urbandictionary.com/v0/define?term="
+UD_GET_URL = "http://api.urbandictionary.com/v0/define"
 UD_RANDOM_URL = "http://api.urbandictionary.com/v0/random"
 
 
@@ -8,14 +8,17 @@ class Model:
     def __init__(self, keyword=None):
         # TODO: logic for when keyword is None
         self.keyword = keyword
-        self.link = UD_GET_URL + self.keyword
-        r = requests.get(self.link)
+        r = requests.get(UD_GET_URL, params={'term': self.keyword})
+        self.link = r.url
         self.data, self.status_code = r.json(), r.status_code
         self.exists = False if self.data[
             'result_type'] == 'no_results' else True
 
     def get_tags(self):
         pass
+
+    def __repr__(self):
+        return "Definitions for: {}".format(self.keyword)
 
 
 if __name__ == '__main__':
